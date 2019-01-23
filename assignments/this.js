f8d806520df9f5f880641f197288c0600cdbe794
Thorben Bender <bender.thorben0@gmail.com>
2019-01-23 16:32:25 +0100

    mvp done

--- assignments/this.js
+++ assignments/this.js
@@ -3,25 +3,40 @@
 *
 * 1. when use this in the global element it points at the root element
 * 2. 
-* 3. 
-* 4. 
+* 3. creating an object with an constructor
+* 4. when you bind an argument to a function, the this will point at the argument
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
-
+this.alert('Help');
 // code example for Window Binding
-this.log('Hello');
 
 // Principle 2
 
 // code example for Implicit Binding
+const Max = {
+    name: 'Max',
+    age: 20,
+    greet: function() {
+        return `Hi, my name is ${this.name}`
+    }
+}
 
 // Principle 3
 
 // code example for New Binding
+function Car(name){
+    this.name = name;
+}
 
 // Principle 4
 
 // code example for Explicit Binding
+
+function callMe(){
+    console.log(`Please call ${this}`);
+}
+const callJames = callMe.bind('James');
+console.log(callJames());
